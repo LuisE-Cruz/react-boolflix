e970a566b6ded3385ae8ce4ae0000a85892f952b
LuisE Cruz <Luis.cruzlc01@gmail.com>
2026-03-26 12:25:44 +0100

    milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,52 @@
+import { useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
 
   const API_KEY = import.meta.env.VITE_API_KEY
 
-  const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=matrix`
+  const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}`
 
-  console.log(API_KEY);
-  console.log(api_url);
+  const [searchValue, setSearchValue] = useState('')
+  const [movieSearched, setMovieSearched] = useState([])
+
+  function searchMovies(e) {
+    e.preventDefault()
+    axios.get(`${api_url}&query=${searchValue}`)
+      .then(response => {
+        setMovieSearched(response.data.results)
+      })
+
+
+  }
 
 
   return (
     <>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-3'>
+            <div className="input-group mb-3 mt-3">
+              <button className="btn btn-outline-secondary" type="button" id="button-addon1" onClick={searchMovies}>Button</button>
+              <input type="text" className="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1" onChange={e => setSearchValue(e.target.value)} />
+            </div>
+            <div>
+              {
 
+                movieSearched.map((movie) => (
+                  <ul key={movie.id}>
+                    <li>{movie.title}</li>
+                    <li>{movie.original_title}</li>
+                    <li>{movie.original_language}</li>
+                    <li>{movie.vote_average}</li>
+                  </ul>
+                ))
+              }
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
