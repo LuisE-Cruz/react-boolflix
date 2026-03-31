1268692e6db5a74c894426794564fa1f04fe653a
LuisE Cruz <Luis.cruzlc01@gmail.com>
2026-03-31 17:17:45 +0200

    milestone 4, improved layout and styling for movie and series display

--- src/App.jsx
+++ src/App.jsx
@@ -49,42 +49,55 @@ function App() {
 
 
   return (
-
-    <div className='container'>
-      <div className='row'>
-        <div className='col-3'>
-          <div className="input-group mb-3 mt-3">
-            <button className="btn btn-outline-secondary" type="button" id="button-addon1" onClick={searchMovies}>Button</button>
-            <input type="text" className="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1" onChange={e => setSearchValue(e.target.value)} />
+    <>
+      <div className='container'>
+        <div className='row bg-dark text-white'>
+          <div className='col-9'>
+            <img className='mt-1 mb-1' src="https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png" alt="Logo Boolfix" />
           </div>
+          <div className='col-3'>
+            <div className="input-group mb-3 mt-3">
+              <button className="btn btn-outline-secondary text-white" type="button" id="button-addon1" onClick={searchMovies}>Button</button>
+              <input type="text" className="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1" onChange={e => setSearchValue(e.target.value)} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='container bg-dark'>
+        <div className='row row-cols-1 row-cols-md-3 row-cols-lg-5 text-white' >
           {
 
             movieSearched.map((movie) => (
-              <ul key={movie.id}>
-                <li>{movie.original_title}</li>
-                <li>{movie.title}</li>
-                <li><img src={movie.poster_path ? `https://image.tmdb.org/t/p/w342/${movie.poster_path}` : 'https://placehold.co/342x513/png?text=Poster+Non+Disponibile'} alt={movie.original_title} /></li>
-                <li className={`fi fi-${ogLanguageToFlag[movie.original_language] || movie.original_language}`}></li>
-                <li>{starsVote(movie.vote_average)}</li>
-              </ul>
+              <div className='col my-3' key={movie.id} id='hover_movies'>
+                <img className='img-fluid' id='movie_display' src={movie.poster_path ? `https://image.tmdb.org/t/p/w342/${movie.poster_path}` : 'https://placehold.co/342x513/png?text=Poster+Non+Disponibile'} alt={movie.original_title} />
+                <div id='movie_overview'>
+                  <h2>{movie.original_title}</h2>
+                  <h3>{movie.title}</h3>
+                  <div className={`fi fi-${ogLanguageToFlag[movie.original_language] || movie.original_language}`}></div>
+                  <div>Voto: {starsVote(movie.vote_average)}</div>
+                  <div><strong>Overview</strong>:{movie.overview}</div>
+                </div>
+              </div>
             ))
           }
-          <hr />
           {
             seriesSearched.map((series) => (
-              <ul key={series.id}>
-                <li>{series.original_name}</li>
-                <li>{series.name}</li>
-                <li><img src={series.poster_path ? `https://image.tmdb.org/t/p/w342/${series.poster_path}` : 'https://placehold.co/342x513/png?text=Poster+Non+Disponibile'} alt={series.original_title} /></li>
-                <li className={`fi fi-${ogLanguageToFlag[series.original_language] || series.original_language}`}></li>
-                <li>{starsVote(series.vote_average)}</li>
-              </ul>
+              <div className='col my-3' key={series.id} id='hover_series'>
+                <img className='img-fluid' id='series_display' src={series.poster_path ? `https://image.tmdb.org/t/p/w342/${series.poster_path}` : 'https://placehold.co/342x513/png?text=Poster+Non+Disponibile'} alt={series.original_name} />
+                <div id='series_overview'>
+                  <h1>{series.original_name}</h1>
+                  <h2>{series.name}</h2>
+                  <div className={`fi fi-${ogLanguageToFlag[series.original_language] || series.original_language}`}></div>
+                  <div>Voto: {starsVote(series.vote_average)}</div>
+                  <div><strong>Overview</strong>:{series.overview}</div>
+                </div>
+              </div>
             ))
           }
         </div>
-      </div>
-    </div>
 
+      </div>
+    </>
   )
 }
 
